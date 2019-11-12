832834f9316e6df15660858a292a10b5ee22eb91
Prateek Malhotra <someone1@gmail.com>
2019-11-12 13:14:24 +0000

    Update Company struct with additional fields
    * Change IssueType to string, helpers exist to convert back to IssueType if required
    
    Sponsored-by: BackendBenchmarking (backendb.com)

--- stocks.go
+++ stocks.go
@@ -173,17 +173,26 @@ type CashFlow struct {
 
 // Company models the company data from the /company endpoint.
 type Company struct {
-	Symbol      string    `json:"symbol"`
-	Name        string    `json:"companyName"`
-	Exchange    string    `json:"exchange"`
-	Industry    string    `json:"industry"`
-	Website     string    `json:"website"`
-	Description string    `json:"description"`
-	CEO         string    `json:"CEO"`
-	IssueType   IssueType `json:"issueType"`
-	Sector      string    `json:"sector"`
-	Employees   int       `json:"employees"`
-	Tags        []string  `json:"tags"`
+	Symbol         string   `json:"symbol"`
+	Name           string   `json:"companyName"`
+	Exchange       string   `json:"exchange"`
+	Industry       string   `json:"industry"`
+	Website        string   `json:"website"`
+	Description    string   `json:"description"`
+	CEO            string   `json:"CEO"`
+	IssueType      string   `json:"issueType"`
+	Sector         string   `json:"sector"`
+	Employees      int      `json:"employees"`
+	Tags           []string `json:"tags"`
+	SecurityName   string   `json:"securityName"`
+	PrimarySICCode string   `json:"primarySicCode"`
+	Address        string   `json:"address"`
+	Address2       string   `json:"address2"`
+	State          string   `json:"state"`
+	City           string   `json:"city"`
+	Zip            string   `json:"zip"`
+	Country        string   `json:"country"`
+	Phone          string   `json:"phone"`
 }
 
 // Dividend models one dividend.
