d1d6e4df572818354817231cc6278c3737597b55
Matthew Rankin <matthew@cumulusware.com>
2020-03-21 09:38:59 -0500

    Update copyright from 2019 to 2019-2020

--- iexdata.go
+++ iexdata.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 The iexcloud developers. All rights reserved.
+// Copyright (c) 2019-2020 The iexcloud developers. All rights reserved.
 // Project site: https://github.com/goinvest/iexcloud
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE file for the project.
