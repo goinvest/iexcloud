0e30a4b2085610730242ccd31ffa727bf2ca6a4a
George Karagoulis <karagog@gmail.com>
2021-12-06 15:19:44 -0800

    Removed redundant go:build line.

--- client_integration_test.go
+++ client_integration_test.go
@@ -3,7 +3,6 @@
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE file for the project.
 
-//go:build integration
 // +build integration
 
 package iex
